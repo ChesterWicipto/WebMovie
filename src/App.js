a265820dd1693a896f0722d7ba473a9bbc847bb1
Chester Wicipto <s21710323@student.unklab.ac.id>
2022-09-14 10:10:34 +0800

    Clear

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Moviebox from "./Moviebox";
 
-function App() {
+const App = () => {
+  const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=367f5b629a0d846efc84ee8735fe85fc";
+  const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=367f5b629a0d846efc84ee8735fe85fc&query";
+  const [movie, setMovie] = useState([]);
+  const [query, setQuery] = useState("");
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${API_URL}/movie/popular`, {
+  //       params: {
+  //         api_key: "367f5b629a0d846efc84ee8735fe85fc",
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data.results);
+  //       setMovie(res.data.results)
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setMovie(data.results);
+      });
+  }, []);
+  // get trending movies for home page
+  // axios.get(API_URL).then((res) => {
+  //   const result = res.results;
+  //   // this.setState({
+  //   //   status: "top-movies",
+  //   //   movies: result,
+  //   // });
+  //   // setMovie(results.results);
+  //   console.log(result);
+  // });
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("Searching ");
+
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=367f5b629a0d846efc84ee8735fe85fc&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovie(data.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand fw-bold" href="#">
+            Movies App
+          </a>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active fw-bold fs-3" aria-current="page" href="/">
+                  Trending
+                </a>
+              </li>
+            </ul>
+            <form class="d-flex " onSubmit={searchMovie}>
+              <input class="form-control me-2" type="search" value={query} onChange={changeHandler} placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div>
+        {movie.length > 0 ? (
+          <div className="content mt-5">
+            <div className="container  ">
+              <div className="card-group row row-cols-1 row-cols-lg-5 g-4 g-lg-5  justify-content-center ">
+                {movie.map((moviereq) => {
+                  // console.log(moviereq);
+                  return (
+                    // <p key={moviereq.id}>{moviereq.title}</p>
+                    <Moviebox key={moviereq.id} {...moviereq} />
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="text-center ">
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1 className="text-dark fw-bold mt-5">Sorry NO Movies Found</h1>
+          </div>
+        )}
+      </div>
+      {/* <div className="container">
+        <div className="grid ">
+          {movie.map((moviereq) => {
+            // console.log(moviereq);
+            return (
+              // <p key={moviereq.id}>{moviereq.title}</p>
+              <Moviebox key={moviereq.id} {...moviereq} />
+            );
+          })}
+        </div>
+      </div> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Moviebox.js
@@ -0,0 +1,63 @@
+// import { Modal, show } from "bootstrap";
+import "./App.css";
+import React, { useState } from "react";
+
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
+
+const Moviebox = ({ title, poster_path, overview, release_date, vote_average }) => {
+  return (
+    // <div className="">
+    //   <img src={API_IMG + poster_path} class="card-img-top" alt="..." />
+    //   <div class="card-body">
+    //     <h5 class="card-title">{title}</h5>
+    //     {/* <p class="card-text">{overview}</p> */}
+    //     <button type="button" className="btn btn-warning">
+    //       View More
+    //     </button>
+    //   </div>
+    // </div>
+    <div className="col bg-dark p-2 ">
+      <div className=" ms-3 mb-3 mt-3 me-3">
+        <div class=" card menu text-center text-white ">
+          <div class="card-body">
+            <img src={API_IMG + poster_path} class="card-img-top" alt="..." />
+            <h5 class="card-title">{title}</h5>
+            {/* <p class="card-text">{overview}</p> */}
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              View All
+            </button>
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class=" modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg ">
+                <div class="modal-content bg-dark">
+                  <div class="modal-header">
+                    <h5 class="modal-title  " id="exampleModalLabel">
+                      <h5 class="card-title">{title}</h5>
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <img src={API_IMG + poster_path} class="img-thumbnail image" alt="..." />
+                    <h3>Score: {vote_average}</h3>
+                    <h4>Release date : {release_date}</h4>
+                    <p>{overview}</p>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-footer">
+            <small>Release on: {release_date}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Moviebox;
